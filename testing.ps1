e8a31e5c6d1f0b800194491d42ee6c6bd8e68e9d
RB <RedBaron2@users.noreply.github.com>
2016-12-20 15:08:23 -0800

    Update testing.ps1

--- testing.ps1
+++ testing.ps1
@@ -1,15 +1,15 @@
 function Get-Version( $URI ) {
-reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /t REG_DWORD /v 1A10 /f /d 0 | out-null
+#reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /t REG_DWORD /v 1A10 /f /d 0 | out-null
 #$URI = “https://www.360totalsecurity.com/en/features/360-total-security/“
 $HTML = Invoke-WebRequest -Uri $URI
 $try = $HTML.Content -split "`n" | sls 'Current version' -Context 2
-Write-Host -$try-
+#Write-Host -$try-
 $try = $try -replace( ' : ',' = ')
 $techy =  ConvertFrom-StringData -StringData $try
 $CurrentVersion = ( $techy.Version )
 Write-Host $techy.Version
-reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v 1A10 /f | out-null
-return $CurrentVersion
+#reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v 1A10 /f | out-null
+#return $CurrentVersion
 $HTML.close
 }
 
