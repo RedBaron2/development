024d283782c557e25edb72eb06aae9d77037836b
RB <RedBaron2@users.noreply.github.com>
2016-12-20 16:25:35 -0800

    Update testing.ps1

--- testing.ps1
+++ testing.ps1
@@ -14,12 +14,15 @@ $HTML.close
 }
 
 function Test {
+reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /t REG_DWORD /v 1A10 /f /d 0 | out-null
     $releases = 'http://www.videosoftdev.com/free-video-editor/download'
     $download_page = Invoke-WebRequest -Uri $releases
     $version = ( $download_page.Content -split '\n'  ) | where { $_ -match '<p><strong>' } | select -First 1
     $version = $version -replace '<p><strong>',''
     $version = $version -replace '</strong></p>',''
     write-host version is -$version-
+reg delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Zones\3" /v 1A10 /f | out-null
+    $download_page.close
 }
 
 Get-360TS-version -URI "https://www.360totalsecurity.com/en/features/360-total-security/"
