a251e803b9c5bd79b7d21ae412e335e1fd77ebd2
RB <RedBaron2@users.noreply.github.com>
2016-12-21 14:13:15 -0800

    Testy

--- invoker.ps1
+++ invoker.ps1
@@ -2,9 +2,9 @@
 $Site = "http://www.videosoftdev.com/free-video-editor"
 $Test = Invoke-WebRequest $Site
 $Testy = $Test.parsedhtml.GetElementsByTagName('p')
-$Testing = $Testy.parsedhtml.GetElementsByTagName('strong') | out-string
+#$Testing = $Testy.parsedhtml.GetElementsByTagName('strong') | out-string
 $i=0
-Foreach ( $s in $Testing ) {
+Foreach ( $s in $Testy ) {
 $i++
-Write-host Testing -$i- -$s-
+Write-host Testy -$i- -$s-
 }
