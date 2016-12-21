b0e4aa4098d088157362346325568a6df067ed88
RB <RedBaron2@users.noreply.github.com>
2016-12-21 14:24:57 -0800

    Out string

--- invoker.ps1
+++ invoker.ps1
@@ -1,8 +1,8 @@
 
 $Site = "http://www.videosoftdev.com/free-video-editor"
 $Test = Invoke-WebRequest $Site
-$Testy = $Test.parsedhtml.GetElementsByTagName('p')
-#$Testing = $Testy.parsedhtml.GetElementsByTagName('strong') | out-string
+$Testy = $Test.parsedhtml.GetElementsByTagName('p')  | out-string
+#$Testing = $Testy.parsedhtml.GetElementsByTagName('strong')
 $i=0
 Foreach ( $s in $Testy ) {
 $i++
