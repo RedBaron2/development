49dd249993d4d2096f877fe805bcbab344af0f7f
RB <RedBaron2@users.noreply.github.com>
2016-12-21 00:00:36 -0800

    Update invoker.ps1

--- invoker.ps1
+++ invoker.ps1
@@ -1,5 +1,6 @@
 
 $Site = "http://www.videosoftdev.com/free-video-editor/download"
 $Test = ( Invoke-WebRequest $Site ).Content
+Write-host A content-$Test-
 $Test = $Test | where { $_ -match '<p><strong>' }
 Write-host content -$Test-
