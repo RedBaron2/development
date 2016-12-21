7f217a07a0c4887303d6353d352094adde052cd7
RB <RedBaron2@users.noreply.github.com>
2016-12-20 17:52:54 -0800

    Update testing.ps1

--- testing.ps1
+++ testing.ps1
@@ -17,7 +17,7 @@ function Test( $releases ) {
 
     $download_page = Invoke-WebRequest -Uri $releases
     $newt = ( $download_page.content -split '\n'  ) | Out-File "C:\projects\development\testing.log"
-    write-host $newt
+    write-host newt -$newt-
     $version = get-content ("C:\projects\development\testing.log") | where { $_ -match '<p><strong>' } | select -First 1
     write-host A version is -$version-
     $version = $version -replace '<p><strong>',''
