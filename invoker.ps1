ace2b7cb223a84ac4a82a06c8178f222bb6a227e
RB <RedBaron2@users.noreply.github.com>
2016-12-21 14:08:13 -0800

    Doubles

--- invoker.ps1
+++ invoker.ps1
@@ -1,5 +1,10 @@
 
 $Site = "http://www.videosoftdev.com/free-video-editor"
 $Test = Invoke-WebRequest $Site
-$Testy= $Test.parsedhtml.GetElementsByTagName('p') | Where { $_ -match '<p><strong>Version' }
-Write-host Testy  -$Testy-
+$Testy = $Test.parsedhtml.GetElementsByTagName('p')
+$Testing = $Testy.parsedhtml.GetElementsByTagName('strong') | out-string
+$i=0
+Foreach ( $s in $Testing ) {
+$i++
+Write-host Testing -$i- -$s-
+}
