8eeea15c5d18c856771dd36e71d0332091daf668
RB <RedBaron2@users.noreply.github.com>
2016-12-22 09:20:48 -0800

    tester1

--- invoker.ps1
+++ invoker.ps1
@@ -1,13 +1,5 @@
 
-$Site = "http://www.videosoftdev.com/free-video-editor"
-$Test = Invoke-WebRequest $Site
-$Testy = $Test.parsedhtml.GetElementsByTagName('p')  | out-string
-#$Testing = $Testy.parsedhtml.GetElementsByTagName('strong')
-$i=0
-Foreach ( $s in $Testy ) {
-$i++
-Write-host Testy -$i- -$s-
-}
+
 
 function Test( $releases ) {
 
