b61909b2eda08c3ff29738304bff90321ada8ed3
RB <RedBaron2@users.noreply.github.com>
2016-12-28 13:53:26 -0800

    Create kso_test.ps1
    created kso_test.ps1

---
+++ kso_test.ps1
@@ -0,0 +1,25 @@
+
+
+$releases = 'https://www.wps.com/office-free'
+
+	  $download_page = Invoke-WebRequest -Uri $releases
+	  $url = ( $download_page.ParsedHtml.getElementsByTagName('a') | Where { $_.className -match 'major transition_color ga-download-2016-free'} ).href
+	  $search='innerHTML';$cpt=1;$resu=@{};
+    $match='(?:Version:)[\d\.]{4,}'; $match_1 = '([WPS]{3}\s[a-zA-Z]{1,}\s\d+\s[Free]{4})';
+    $replace='Version:';
+	  $download_page.ParsedHtml.getElementsByTagname("a") |%{
+    if ($_.id -eq $null){ $_.id="p$cpt";$cpt++};$resu[$($_.id)]=$_.$search
+    #$resu
+    if ( $_.$search -match($match) ) { $version= $_.$search; }
+    if ( $_.$search -match($match_1) ) { $product = $_.$search; }    }
+    $description = ( $download_page.ParsedHtml.getElementsByTagName('div') | Where { $_.className -match 'product'} ).innertext
+    $description = $description -replace('WPS Office for ','')
+    $description = '<![CDATA[' + $description + ']]>'
+    Write-Host $description
+	  $version = $version -match($match);$version = $matches[0];
+    $version = $version -replace($replace)
+    $product = $product -match($match_1);$product = $matches[0]; 
+    $productName = $product -replace('\s[\d+]{4}\s',' ')
+    $productName = $productName -replace('\s','-')
+
+    return @{ URL32 = $url; Version = $version; packageName = $productName.ToLower() ; softwareName = $product.ToLower() ; description = $description }
