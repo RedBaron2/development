69da468dd067f793e66a076e29751ffe49de73d3
RB <RedBaron2@users.noreply.github.com>
2017-03-18 01:10:13 -0700

    Update ChocolateyInstall.ps1
    Testing

--- automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
+++ automatic/flashplayerppapi/tools/ChocolateyInstall.ps1
@@ -3,7 +3,7 @@ $packageName = 'flashplayerppapi'
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'msi'
-  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/25.0.0.127/install_flash_player_25_ppapi.msi'
+  url           = 'https://download.macromedia.com/pub/flashplayer/pdc/24.0.0.127/install_flash_player_24_ppapi.msi'
   silentArgs    = '/quiet /norestart REMOVE_PREVIOUS=YES'
   softwareName  = 'Flash Player PPAPI'
   checksum      = '1E1ACC98FEEFCD666F19F191DB29250529DC16B2ED5FE6FA9CA303EFC7D2103C'
