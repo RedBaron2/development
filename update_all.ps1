ca6c265a248629b51fe6c61d813bbea07e2be14d
RB <RedBaron2@users.noreply.github.com>
2017-03-18 18:24:13 -0700

    Update update_all.ps1

--- update_all.ps1
+++ update_all.ps1
@@ -2,4 +2,4 @@
 cd .\automatic
 Get-ChildItem */* | 
 where {$_.name -eq "update.ps1"} | 
-foreach { cd $_.DirectoryName; ".\update.ps1"; cd ..}
+foreach { cd $_.DirectoryName; $_ + ".\update.ps1"; cd ..}
