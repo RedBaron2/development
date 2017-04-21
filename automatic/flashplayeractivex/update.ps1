d586f87743a56a06bea636868313bfb75fae89f0
RB <RedBaron2@users.noreply.github.com>
2017-04-20 21:31:12 -0700

    Update update.ps1
    xml to save the day

--- automatic/flashplayeractivex/update.ps1
+++ automatic/flashplayeractivex/update.ps1
@@ -27,13 +27,28 @@ function global:au_SearchReplace {
 
 function global:au_GetLatest {
 
-  $HTML = Invoke-WebRequest -Uri $releases
-  $try = ($HTML.ParsedHtml.getElementsByTagName('p') | Where{ $_.className -eq 'NoBottomMargin' } ).innerText
-  $try = $try  -split "\r?\n"
-  $try = $try[0] -replace ' ', ' = '
-  $try =  ConvertFrom-StringData -StringData $try
-  $CurrentVersion = ( $try.Version )
-  $majorVersion = ([version] $CurrentVersion).Major
+
+  # $HTML = Invoke-WebRequest -Uri $releases
+  # $try = ($HTML.ParsedHtml.getElementsByTagName('p') | Where{ $_.className -eq 'NoBottomMargin' } ).innerText
+  # $try = $try  -split "\r?\n"
+  # $try = $try[0] -replace ' ', ' = '
+  # $try =  ConvertFrom-StringData -StringData $try
+  # $CurrentVersion = ( $try.Version )
+  # $majorVersion = ([version] $CurrentVersion).Major
+
+  # $url32 = "https://download.macromedia.com/pub/flashplayer/pdc/${CurrentVersion}/install_flash_player_${majorVersion}_active_x.msi"
+
+  # $packageVersion = Get-Padded-Version $CurrentVersion $padVersionUnder
+
+  # return @{ URL32 = $url32; Version = $packageVersion; RemoteVersion = $CurrentVersion; majorVersion = $majorVersion; }
+# }
+
+# update -ChecksumFor none
+
+  $XML = New-Object  System.Xml.XmlDocument
+  $XML.load($releases)
+  $currentVersion = $XML.XML.update.version.replace(',', '.')
+  $majorVersion = ([version]$currentVersion).Major
 
   $url32 = "https://download.macromedia.com/pub/flashplayer/pdc/${CurrentVersion}/install_flash_player_${majorVersion}_active_x.msi"
 
@@ -43,3 +58,4 @@ function global:au_GetLatest {
 }
 
 update -ChecksumFor none
+
