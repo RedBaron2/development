dd435b92e09da52d4f8b92ed6871c7dc745d4a41
RB <RedBaron2@users.noreply.github.com>
2017-04-25 09:11:06 -0700

    Create packageArgs.ps1
    packageArgs.ps1

---
+++ automatic/jre8/tools/packageArgs.ps1
@@ -0,0 +1,17 @@
+# Due to a big in AU. This package requires that all package Arguments be in this hashtable
+
+$packageArgs = @{
+	oldVersion		= '8.0.1210.13'
+	version			= '8.0.1310.11'
+	packageName		= 'jre8'
+	fileType		= 'exe'
+	url				= 'http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220313_d54c1d3a095b4ff2b6607d096fa80163'
+	url64bit		= 'http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220315_d54c1d3a095b4ff2b6607d096fa80163'
+	silentArgs		= '/s REBOOT=0 SPONSORS=0 REMOVEOUTOFDATEJRES=1'
+	validExitCodes	= @(0)
+	softwareName  	= 'Java SE Runtime Environment'
+	checksum		= '73BF9257E2F4CA73318D3C23181CBE1E93665BF13FDA7B956252A70B975BCF8B'
+	checksumType	= 'sha256'
+	checksum64		= '5083590A30BF069E947DCE8968221AF21B39836FE013B111DE70D6107B577CD3'
+	checksumType64	= 'sha256'
+}
