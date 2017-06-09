60fc181919be1b207136b25fa22f277e7da94eac
RB <RedBaron2@users.noreply.github.com>
2017-06-09 09:44:50 -0700

    Update update_all.ps1

--- update_all.ps1
+++ update_all.ps1
@@ -6,7 +6,7 @@ $ErrorActionPreference = "silentlycontinue"
 # where {$_.name -eq "update.ps1"} | 
 # foreach { cd $_.DirectoryName; .\update.ps1; cd ..}
 
-cd ..
+#cd ..
 cd .\test
 .\testing.ps1
 # updateall
