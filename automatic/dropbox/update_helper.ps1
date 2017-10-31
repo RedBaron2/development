79de9a9defa34f75266108dd337d0111b2bce098
RB <RedBaron2@users.noreply.github.com>
2017-10-31 15:00:44 -0700

    Update update_helper.ps1
    from core with mod

--- automatic/dropbox/update_helper.ps1
+++ automatic/dropbox/update_helper.ps1
@@ -1,52 +1,26 @@
 
 function drpbx-compare {
-	param(
-		[string]$_version,
-		[string]$build = 'stable',
-		[bool]$_build
-	)
+	param( 
+[Parameter(Position = 0)][string]$_version, [string]$build = 'stable' )
     $releases = 'https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016'
-    $weblinks = "${env:temp}\${build}links.txt"
-    $HTML = ( Invoke-WebRequest -UseBasicParsing -Uri $releases).Links`
-     | where {($_ -match ($build) )} | Select -First 10 | Out-File $weblinks
-    $hrefs = ( Get-Content $weblinks )
-
+    $HTML = (Invoke-WebRequest -UseBasicParsing -Uri $releases).Links`
+     | where {($_ -match $build)} | Select -First 6 | out-string
     $re_dash = '-'; $re_dot = '.'; $re_non = ''; $re_build = $build + "-Build-";
-    $version = ( drpbx-builds -hrefs $hrefs -testVersion $_version -_build $_build )
-    rm $weblinks
+    $version = (drpbx-builds -hrefs $HTML -testVersion $_version);
     return $version
 }
 
 function drpbx-builds {
-	param(
-		[string]$default = '27.3.21',
-		[string]$hrefs,
-		[string]$testVersion,
-		[bool]$_build
-	)
-    $links = $hrefs
-    $links = $links -split ( '\/' ); $build = @()
-    $regex = @{$true=($re_build);$false=($re_build)}[ ($_build) ]
-    foreach( $_ in $links ) {
-        foreach( $G in $_ ) {
-            if ( $G -match '([\d]{2}[\-]{1}[\d]{1,2}[\-]{1}[\d]{2})' ) {
-                $G = $G -replace($regex,$re_non) -replace($re_dash, $re_dot)
-                    if ( $G -ge $default ) {
-                        if ($_build) {
-                            if (( $G -ge $testVersion )) {
-                            $build += $G + ";";
-                            if (( $build | measure ).Count -ge '6') { $build = ($build | measure -Maximum ).Maximum; break;}
-                            }
-                        } else {
-                            if (( $G -le $testVersion )) {
-                            $build += $G + ";";
-                            if (( $build | measure ).Count -ge '6') { $build = ($build | measure -Maximum ).Maximum; break;}
-                            }
-                        }
-                    }
-                }
+	param( [string]$default = '27.3.21', [string]$hrefs, [string]$testVersion )
+    $links = $hrefs -split ( '\/' ); $build = @(); $regex = ($re_build);
+    foreach($_ in $links) {
+        foreach($G in $_) {
+            if ($G -match '([\d]{2}[\-]{1}[\d]{1,2}[\-]{1}[\d]{2})') {
+                $G = $G -replace($regex,$re_non) -replace($re_dash,$re_dot) -replace('New.',$re_non);
+					if (($G -ge $default) -and ($G -ge $testVersion)) { $build += $G + ";"; }
+					}
+			if (($build | measure).Count -ge '6') { $build = ($build | measure -Maximum).Maximum; break; }
             }
-    }
-	return (( $build -split(";")) | Select -First 1)
+        }
+	return ($build -split(";") | select -First 1)
 }
-
