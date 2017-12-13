bf06157fa87724fdc12777293728f7f40f92ea45
RB <RedBaron2@users.noreply.github.com>
2017-12-13 15:32:36 -0800

    Update update.ps1
    Stream Build

--- automatic/360ts/update.ps1
+++ automatic/360ts/update.ps1
@@ -1,29 +1,58 @@
 
 import-module au
 
-$URL32 = 'http://int.down.360safe.com/totalsecurity/360TS_Setup.exe'
-$releases = 'https://www.360totalsecurity.com/en/download-free-antivirus/360-total-security/?offline=1'
+function global:au_BeforeUpdate {
+  if ($Latest.Title -like '*essential*') {
+    cp "$PSScriptRoot\README_tse.md" "$PSScriptRoot\README.md" -Force
+  } else {
+    cp "$PSScriptRoot\README_ts.md" "$PSScriptRoot\README.md" -Force
+  }
+}
 
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
+      "(^\s*packageName\s*=\s*)('.*')" = "`$1'$($Latest.PackageName)'"
       "(^\s*url\s*=\s*)('.*')" = "`$1'$($Latest.URL32)'"
       "(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
       "(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(^\s*softwareName\s*=\s*)('.*')" = "`$1'$($Latest.Title)'"
+    }
+    ".\360ts.nuspec" = @{
+      "(?i)(^\s*\<title\>).*(\<\/title\>)" = "`${1}$($Latest.Title)`${2}"
     }
   }
 }
 
+function Get360Version {
+param(
+	[string]$releases,
+	[string]$PackageName,
+	[string]$Title
+)
+  $HTML = Invoke-WebRequest -UseBasicParsing -Uri $releases
+  $url = $HTML.Links | ? href -match "\.exe$" | select -first 1 -expand href
+  $version = $url -split '_|(\.exe)' | select -last 1 -skip 2
+
+  $url = 'https:' + $url
+ @{
+    PackageName = $PackageName
+    Title = $Title
+    Version = $version
+    URL32   = $url
+  }
+}
+
+$360tsURL = 'https://www.360totalsecurity.com/en/download-free-antivirus/360-total-security/?offline=1'
+$360tseURL = 'https://www.360totalsecurity.com/en/download-free-antivirus/360-total-security-essential/?offline=1'
+
 function global:au_GetLatest {
+  $streams = [ordered] @{
+    tse = Get360Version -releases $360tseURL -PackageName "360tse" -Title "360 Total Security Essential"
+    ts = Get360Version -releases $360tsURL -PackageName "360ts" -Title "360 Total Security"
+  }
 
-$HTML = Invoke-WebRequest -UseBasicParsing -Uri $releases
-$links = $HTML.Links | where{ ($_.href -match "360TS_Setup" ) } | Select -first 1
-$link = $links.href -split ( '\/' )
-$name = $link | Select -Last 1
-$ver = $name -replace('.exe','')
-$version = $ver -split ( '_' )
-
-  return @{ URL32 = $URL32; Version = ( $version | Select -Last 1 ); }
+  return @{ Streams = $streams }
 }
 
 update -ChecksumFor 32
