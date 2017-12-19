03037f59490db49c1756ddfeb93cd2c54e39f4c6
RB <RedBaron2@users.noreply.github.com>
2017-12-19 14:53:11 -0800

    Add files via upload

--- automatic/chromium/tools/chocolateyInstall.ps1
+++ automatic/chromium/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+﻿$ErrorActionPreference = 'Stop'
 
 $chromium_string = "\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Chromium"
 $hive = "hkcu"
@@ -11,12 +11,32 @@ if (Test-Path $Chromium) {
 }
 
 
+function Get-CompareVersion {
+  param(
+    [string]$version,
+    [string]$notation,
+    [string]$package
+  )
+    $version = $version -replace($notation,"")
+    [array]$key = Get-UninstallRegistryKey -SoftwareName "$package*"
+    if ($version -eq ( $key.Version )) {
+      Write-Host "$package $version is already installed."
+      return
+    }
+  }
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$version = "63.0.3239.108"
+if ( Get-ProcessorBits -eq 32 ) {
+$file = "chromium_x32.exe.ignore"
+} else {
+$file = "chromium_x64.exe.ignore" }
+New-Item "$toolsDir\$file" -ItemType file
+Get-CompareVersion -version $version -notation "-snapshots" -package "chromium"
 
 $packageArgs = @{
   packageName   = 'chromium'
-  file          = "$toolsdir\chromium-sync_x32.exe"
-  file64        = "$toolsdir\chromium-sync_x64.exe"
+  file          = "$toolsdir\chromium_x32.exe"
+  file64        = "$toolsdir\chromium_x64.exe"
   fileType      = 'exe'
   silentArgs    = $silentArgs
   validExitCodes= @(0)
