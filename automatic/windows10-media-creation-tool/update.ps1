c8f004a3267e8122e3fe4bc9bf81e55c34c00942
RB <RedBaron2@users.noreply.github.com>
2017-12-19 16:38:11 -0800

    Update update.ps1

--- automatic/windows10-media-creation-tool/update.ps1
+++ automatic/windows10-media-creation-tool/update.ps1
@@ -20,14 +20,16 @@ param(
     [string]$url,
     [string]$file
 )
+    $regex = "((\d+.\d+.\d+.\d+))"
     $packageName = $($Latest.PackageName)
     if (!(Test-Path "${env:temp}\chocolatey\$packageName" -PathType Container)) {
     New-Item -ItemType Directory "${env:temp}\chocolatey\$packageName" }
     Invoke-WebRequest -Uri $url -OutFile "${env:temp}\chocolatey\$packageName\$file"
     $filer = Get-Item "${env:temp}\chocolatey\$packageName\*.exe" | select -First 1
-	$version = $filer.VersionInfo.FileVersion -replace '((\d+.\d+.\d+.\d+))*','$1'
+	$version = $filer.VersionInfo.FileVersion -replace '$regex*','$1'
     if (( $version -match " " )) { $like=$true;$version = $version -split(" ") }
     $version = @{$true=$version;$false=$version[0]}[ $version -isnot [system.array] ]
+    $version = $version -match $regex; $version = $Matches[0]
     return $version
 }
 
