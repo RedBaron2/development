5fddc401f08b2430ad841ed62c7409a0aa13f003
RB <RedBaron2@users.noreply.github.com>
2017-12-26 18:11:32 -0800

    Update and rename automatic/windowsrepair.portable/tools/chocolateyInstall.ps1 to automatic/windowsrepair/tools/chocolateyInstall.ps1
    win repair stream

--- automatic/windowsrepair/tools/chocolateyInstall.ps1
+++ automatic/windowsrepair/tools/chocolateyInstall.ps1
@@ -1,12 +1,11 @@
-﻿$packageArgs = @{
+$packageArgs = @{
   packageName    = 'windowsrepair'
-  fileType       = 'zip'
+  fileType       = 'exe'
   url            = 'http://www.tweaking.com/files/setups/tweaking.com_windows_repair_aio.zip'
-  UnzipLocation	 = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
   checksum       = '521776ad02dd690580bd6f322f1e2d36aea267cc1ce32df19aae4a2a8884431f'
   checksumType   = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
-  softwareName   = 'Windows Repair All in One (Portable)'
+  softwareName   = 'Windows Repair All in One'
 }
-Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyPackage @packageArgs
