8a3a8d5cdd15f624e7f86be5fa1c32500956a90c
RB <RedBaron2@users.noreply.github.com>
2019-05-14 17:37:25 -0700

    Update update_helper.ps1
    edited for code cleanup

--- automatic/wps-office-free/update_helper.ps1
+++ automatic/wps-office-free/update_helper.ps1
@@ -26,8 +26,6 @@ function Get-JavaSiteUpdates {
   [string]$wait = 4
  )
  
-$OS_caption = ( Get-CimInstance win32_operatingsystem -Property Caption )
-$check = @{$true=$true;$false=$false}[ ( $OS_caption -match 'Server' ) ]
 $regex = '([\d]{0,2}[\.][\d]{0,2}[\.][\d]{0,2}[\.][\d]{0,5})'
 $url = Get-PackageName $package
 $ie = New-Object -comobject InternetExplorer.Application
@@ -36,29 +34,17 @@ $ie.Visible = $false
 while($ie.ReadyState -ne $wait) {
  start-sleep -Seconds 20
 }
-	if ( $check ) {
-		$url32 = $ie.Document.IHTMLDocument3_getElementsByTagName("a") | % { $_.href } | where { $_ -match $regex } | select -First 1
-	} else {
-		$url32 = $ie.Document.getElementsByTagName("a") | % { $_.href } | where { $_ -match $regex } | select -First 1
-	}
   
     foreach ( $_ in $ie.Document.getElementsByTagName("a") ) {
      $url = $_.href;
          if ( $url -match $regex) {
             $yes = $url | select -last 1
             $version = $Matches[0]
-            $the_match = $yes -match( $rev_regex );
-            $revision = $Matches[0];
             break;
         }
     }
 $ie.quit()
-if ($package -match 'wps') {
 $version = $version
-} else {
-$version = $version + $revision
-}
-
 
 	@{    
 		PackageName = $package
