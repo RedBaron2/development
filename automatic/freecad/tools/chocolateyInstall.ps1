1cd7bea8fa01056ca93ae985f1d8e6a20cb4b37b
RB <RedBaron2@users.noreply.github.com>
2019-12-27 19:31:19 -0800

    Add files via upload
    update
    install
    uninstall
    helper (new)

--- automatic/freecad/tools/chocolateyInstall.ps1
+++ automatic/freecad/tools/chocolateyInstall.ps1
@@ -1,62 +1,33 @@
-$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-
-# Checking for Package Parameters
-$pp = Get-PackageParameters
-if (!$pp['UnzipLocation']) { $pp['UnzipLocation'] = "$toolsDir" }
+ if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+. "$PSScriptRoot\helper.ps1"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = ''
+  fileType       = '7z'
   url            = ''
-  url64          = ''
-  UnzipLocation	 = $pp.UnzipLocation
+  url64          = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.19_pre/FreeCAD_0.19.19093_x64_Conda_Py3QT5-WinVS2015.7z'
   softwareName   = 'FreeCAD*'
   checksum       = ''
   checksumType   = ''
-  checksum64     = ''
-  checksumType64 = ''
+  checksum64     = '5F82D81FD6B2CC4CA898FF28F711FA360920E783BE408A6B0C885C19B0CFF754'
+  checksumType64 = 'sha256'
   silentArgs     = '/S'
   validExitCodes = @(0)
 }
 
-$fileName = @{$true=($packageArgs.url);$false=($packageArgs.url64)}[ ((Get-OSBitness) -eq 32) ]
-$filename = $fileName -split('/'); $filename = ( $filename[-1] ); $filename = ( $filename -replace( "\.$($packageArgs.fileType)", '' ) )
-
-# Checking for Package Parameters
-if (!$pp['WorkingDirectory']) { $pp['WorkingDirectory'] = $pp.UnzipLocation+"\$filename" }
-if (!$pp['TargetPath']) { $pp['TargetPath'] = $pp['WorkingDirectory']+"\bin\${env:ChocolateyPackageTitle}.exe" }
-if (!$pp['IconLocation']) { $pp['IconLocation'] = $pp['TargetPath'] }
-if (!$pp['Arguments']) { $pp['Arguments'] = "" }
-if (!$pp['ShortcutFilePath']) { $pp['ShortcutFilePath'] = ( [Environment]::GetFolderPath('Desktop') )+"\${env:ChocolateyPackageTitle}.lnk" }
-if (!$pp['Shortcut']) { $pp['Shortcut'] = $true }
-if (!$pp['WindowStyle']) { $pp['WindowStyle'] = 1 }
-
-$packageParams = @{
-  ShortcutFilePath = $pp.ShortcutFilePath
-  TargetPath = $pp.TargetPath
-  WorkingDirectory = $pp.WorkingDirectory
-  Arguments = $pp.Arguments
-  IconLocation = $pp.IconLocation
-  Description = "FreeCAD Development ${env:ChocolateyPackageVersion}"
-  WindowStyle = $pp.WindowStyle
-}
-
-if ($pp['Taskbar']) { $packageParams.Add( 'PinToTaskbar', $true ) }
-if ($pp['Admin']) {  $packageParams.Add( 'RunAsAdmin', $true ) }
-
 if ( $packageArgs.filetype -eq '7z' ) {
-Install-ChocolateyZipPackage @packageArgs
-$files = get-childitem $pp.WorkingDirectory -Exclude $packageArgs.softwareName -include *.exe -recurse
-foreach ($file in $files) {
-  #generate an ignore file
-  New-Item "$file.ignore" -type file -force | Out-Null
-}
-if ( $pp.Shortcut ) { Install-ChocolateyShortcut @packageParams }
+  # Checking for Package Parameters
+  $pp = ( Get-UserPackageParams -scrawl )
+  if ($pp.UnzipLocation) { $packageArgs.Add( "UnzipLocation", $pp.UnzipLocation ) }
+  Install-ChocolateyZipPackage @packageArgs
+  if ($pp.Shortcut) { Install-ChocolateyShortcut @pp }
+  $files = get-childitem $pp.WorkingDirectory -Exclude $packageArgs.softwareName -include *.exe -recurse
+  foreach ($file in $files) {
+    # Generate an ignore file(s)
+    New-Item "$file.ignore" -type file -force | Out-Null
+  }
 } else {
-Install-ChocolateyPackage @packageArgs
+  Install-ChocolateyPackage @packageArgs
 }
-
-# Exporting Package Parameters to json file in toolsdir
-$packageParams | ConvertTo-Json | Out-File  ( "$toolsDir\pp.json" )
