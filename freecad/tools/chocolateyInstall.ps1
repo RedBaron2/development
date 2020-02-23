e7c52c0faad283d656a39db55cf3c5aae499e861
RB <RedBaron2@users.noreply.github.com>
2020-02-22 19:50:56 -0800

    Add files via upload
    fixing a bug

---
+++ freecad/tools/chocolateyInstall.ps1
@@ -0,0 +1,33 @@
+﻿$ErrorActionPreference = 'Stop';
+
+ if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+. "$PSScriptRoot\helper.ps1"
+
+$packageArgs = @{
+  packageName    = ''
+  fileType       = ''
+  url            = ''
+  url64          = ''
+  softwareName   = ''
+  checksum       = ''
+  checksumType   = ''
+  checksum64     = ''
+  checksumType64 = ''
+  silentArgs     = '/S'
+  validExitCodes = @(0)
+}
+
+if ( $packageArgs.filetype -eq '7z' ) {
+  # Checking for Package Parameters
+  $pp = ( Get-UserPackageParams -scrawl )
+  if ($packageArgs.url64 -match "Conda") { $packageArgs.Remove("url"); $packageArgs.Remove("checksum"); $packageArgs.Remove("checksumType"); }
+  if ($pp.UnzipLocation) { $packageArgs.Add( "UnzipLocation", $pp.UnzipLocation ) }
+  Install-ChocolateyZipPackage @packageArgs
+  if ($pp.NoShortcut) { $pp.Remove("Shortcut"); Install-ChocolateyShortcut @pp }
+  $files = get-childitem $pp.WorkingDirectory -Exclude $packageArgs.softwareName -include *.exe -recurse
+  foreach ($file in $files) {
+    New-Item "$file.ignore" -type file -force | Out-Null # Generate an ignore file(s)
+  }
+} else {
+  Install-ChocolateyPackage @packageArgs
+}
