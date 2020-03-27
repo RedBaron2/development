b95d65eb3435ffdf88b7fe33c592c2577d28d261
RB <RedBaron2@users.noreply.github.com>
2020-03-26 18:23:04 -0700

    Add files via upload

---
+++ automatic/waterfox/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+if ((Get-OSArchitectureWidth -Compare 32) -or $env:chocolateyForceX86 -eq $true) {
+  throw "Waterfox do not support 32bit (x86) installation."
+}
+
+$packageArgs = @{
+  packageName   = 'waterfox'
+  fileType      = 'exe'
+  softwareName  = 'Waterfox*'
+  file          = "$toolsDir\Waterfox Current 2020.03 Setup_x64.exe"
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Remove-Item $packageArgs.file -Force -ErrorAction SilentlyContinue
---
+++ automatic/waterfox/update.ps1
@@ -0,0 +1,70 @@
+import-module au
+$releases = 'https://www.waterfox.net/releases/'
+$softwareName = 'Waterfox*'
+
+function global:au_BeforeUpdate {
+  if ($Latest.Title -like '*Classic*') {
+    cp "$PSScriptRoot\readme.classic.md" "$PSScriptRoot\readme.md" -Force
+  }
+  else {
+    cp "$PSScriptRoot\readme.current.md" "$PSScriptRoot\readme.md" -Force
+  }
+  
+  $Latest.ChecksumType64 = 'sha256'
+  $fileName = $Latest.URL64 -split '/' | select -last 1
+  $fileName = ($fileName -replace '%20',' ').TrimEnd('.exe')
+  Get-RemoteFiles -Purge -FileNameBase $fileName
+  $Latest.FileName64 = $fileName + "_x64.exe"
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\legal\VERIFICATION.txt" = @{
+      "(?i)(listed on\s*)\<.*\>" = "`${1}<$releases>"
+      "(?i)(1\..+)\<.*\>"        = "`${1}<$($Latest.URL64)>"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType64)"
+      "(?i)(checksum:).*"        = "`${1} $($Latest.Checksum64)"
+    }
+
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(`"`[$]toolsDir\\).*`""        = "`${1}$($Latest.FileName64)`""
+      "(?i)(^\s*softwareName\s*=\s*)'.*'" = "`${1}'$softwareName'"
+      "(?i)(^\s*packageName\s*=\s*)'.*'"  = "`${1}'$($Latest.PackageName)'"
+    }
+    ".\waterfox.nuspec"  = @{
+      "(?i)(^\s*\<id\>).*(\<\/id\>)" = "`${1}$($Latest.PackageName)`${2}"
+      "(?i)(^\s*\<title\>).*(\<\/title\>)" = "`${1}$($Latest.Title)`${2}"
+    }
+  }
+}
+
+function Get-Waterfox {
+param(
+    [string]$build
+)
+  $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+  $re    = "(?:Waterfox%20$build%20)([\d]{0,4}[\.][\d]{0,2})(%20Setup\.[ex]+)|(?:Waterfox%20$build%20)([\d]{0,4}[\.][\d]{0,2}[\.][\d]{0,2})(%20Setup\.[ex]+)"
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+  if (!$url) {
+    $re = 'Setup\.exe$' # If we didn't get a url with the previous regex, we use a much simpler way
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+  }
+
+  $version  = $url -split '%20| ' | select -Last 1 -Skip 1
+  if ($build -eq 'Classic') { $build = 'Classic'; $dash = '-' } else { $build=$dash = '' }
+  # We need to replace the space in the url, otherwise we'll get an invalid url error.
+  return @{ PackageName = "Waterfox$dash$build" ; Title = "Waterfox $build" ; URL64 = ($url -replace ' ','%20'); Version = $version }
+}
+
+function global:au_GetLatest {
+
+  $streams = [ordered] @{
+    classic = Get-Waterfox -build "Classic"
+    current = Get-Waterfox -build "Current"
+  }
+
+  return @{ Streams = $streams }
+}
+
+update -ChecksumFor none
