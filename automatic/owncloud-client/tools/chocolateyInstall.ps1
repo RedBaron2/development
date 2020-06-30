b6ecf6fa18bd5e4ac08e8197bd65df2b4b43696e
RB <RedBaron2@users.noreply.github.com>
2020-06-30 10:18:09 -0700

    owncloud-fix

---
+++ automatic/owncloud-client/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsPath = Split-Path -parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  file           = "$toolsPath\ownCloud-2.6.3.14058.13619.msi"
+  softwareName   = 'ownCloud'
+  silentArgs     = '/qn /norestart'
+  validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
+
+Get-ChildItem $toolsPath\*.msi | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
