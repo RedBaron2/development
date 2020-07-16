1f519e95c49e41e504ba04c4e880281b88664d62
RB <RedBaron2@users.noreply.github.com>
2020-07-16 16:55:33 -0700

    testing extra

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -11,6 +11,6 @@ $packageArgs = @{
   validExitCodes = @(0)
 }
 
-if ([string]::IsNullOrEmpty($packageArgs.silentArgs)) { $packageArgs.silentArgs = "--silent --help" }
+if ([string]::IsNullOrEmpty($packageArgs.silentArgs)) { $packageArgs.silentArgs = "--silent --" }
 
 Install-ChocolateyPackage @packageArgs
