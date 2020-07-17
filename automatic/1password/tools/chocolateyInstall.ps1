4364912e52a4e86cfc1a8a46fd4152d5af533a9a
RB <RedBaron2@users.noreply.github.com>
2020-07-16 18:30:10 -0700

    kill process lines

--- automatic/1password/tools/chocolateyInstall.ps1
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -8,8 +8,12 @@ $packageArgs = @{
   checksum       = ''
   checksumType   = ''
   silentArgs     = ''
-  validExitCodes = @(0)
+  validExitCodes = @(0 , 1)
 }
 
-if ([string]::IsNullOrEmpty($packageArgs.silentArgs)) { $packageArgs.silentArgs = "--help" }
- Install-ChocolateyPackage @packageArgs
+if ([string]::IsNullOrEmpty($packageArgs.silentArgs)) { $packageArgs.silentArgs = "--silent" }
+ Install-ChocolateyPackage @packageArgs -Verbose
+ 
+ sleep 60
+ $processID = ( Get-Process $packageArgs.packageName ).Id
+ if ( $processID ) { Stop-Process -Id $processID }
