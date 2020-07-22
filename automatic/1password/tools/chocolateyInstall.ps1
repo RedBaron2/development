f7734bf68ce6783b7e479ecb7abb90d1d47029be
RB <RedBaron2@users.noreply.github.com>
2020-07-22 12:26:58 -0700

    1password newer
    This will upload the 1password package to the repo

---
+++ automatic/1password/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'exe'
+  url            = ''
+  softwareName   = '1Password*'
+  checksum       = ''
+  checksumType   = ''
+  silentArgs     = ''
+  validExitCodes = @(0)
+}
+# Download from an HTTPS location
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = Get-ChocolateyWebFile @packageArgs -FileFullPath "$toolsDir\PassWord_Install.exe"
+
+Start-Process -FilePath $filePath -ArgumentList $packageArgs.silentArgs
+sleep 12
+Remove-Item "$toolsDir\PassWord_Install.exe" -force
---
+++ automatic/1password/tools/chocolateyUninstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop'
+
+[array]$key = ( Get-UninstallRegistryKey -SoftwareName "${env:ChocolateyPackageName}*" ).UninstallString
+$file = ($key -split " ")[0]
+
+Write-Warning "This will stop $env:ChocolateyPackageName before Chocolatey Auto Uninstall completes."
+$proc = ( Get-Process $env:ChocolateyPackageName ).Id
+Stop-Process -Id $proc
+$filepath,$arguments = ($key -split " ")[0]
+if ($arguments -eq $null) { Write-Warning "arguments is null"; $arguments = "uninstall" }
+Write-Warning "The fp -$filepath- arg -$arguments-"
+sleep 10; Start-Process -FilePath $filepath -ArgumentList $arguments
