fc2ecd355b64ce0c8609d84cfec06d87a6c72ab1
RB <RedBaron2@users.noreply.github.com>
2020-10-09 14:50:39 -0700

    libreoffice streams package
    This is a test

---
+++ automatic/libreoffice-streams/tools/chocolateyInstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+. $toolsDir\helpers.ps1
+
+$packageArgs = @{
+  packageName            = 'libreoffice-fresh'
+  version                = '5.0.0'
+  fileType               = 'msi'
+  url                    = 'https://downloadarchive.documentfoundation.org/libreoffice/old/5.0.0.5/win/x86/LibreOffice_5.0.0.5_Win_x86.msi'
+  url64bit               = 'https://downloadarchive.documentfoundation.org/libreoffice/old/5.0.0.5/win/x86_64/LibreOffice_5.0.0.5_Win_x64.msi'
+  checksum               = '633f739902dfccc7a2f915ee874dff80c050fc3667c4d56a0d674821ff11b362'
+  checksum64             = '20e8373de8e9aadf9d194ea1d9b95b38fa54ecd4e91f3fe18df1fc02e036bd16'
+  checksumType           = 'sha256'
+  checksumType64         = 'sha256'
+  silentArgs             = '/qn /passive /norestart /l*v "{0}"' -f "$($env:TEMP)\$($env:ChocolateyPackageName).$($env:ChocolateyPackageVersion).MsiInstall.log"
+  validExitCodes         = @(0,3010)
+  softwareName           = 'LibreOffice*'
+}
+
+if (-not (IsUrlValid $packageArgs.url)) {
+  $exactVersion = GetLibOExactVersion $packageArgs.version
+  $packageArgs.url = $exactVersion.Url32
+  $packageArgs.url64bit = $exactVersion.Url64
+}
+
+Install-ChocolateyPackage @packageArgs
