5719e7dcdac609e0fe0fd38666dabc8327e0058c
RB <RedBaron2@users.noreply.github.com>
2021-03-24 09:40:17 -0700

    cygwin package for testing
    of a install dir with space

---
+++ automatic/Cygwin/tools/chocolateyUninstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$cygwin_root = (Get-ItemProperty 'HKLM:\SOFTWARE\Cygwin\setup' -ea 0).rootdir
+Remove-Item 'HKLM:\SOFTWARE\Cygwin' -Recurse -ErrorAction SilentlyContinue
+if (Test-Path "$cygwin_root\Cygwin.bat") {
+    Write-Host 'Removing cygwin files'
+    Remove-Item $cygwin_root -recurse -force -ea 0
+}
+
+Uninstall-BinFile -Name Cygwin
