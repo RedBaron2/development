89174d1846f9a92159ece64e70d8b090857af55a
RB <RedBaron2@users.noreply.github.com>
2021-03-24 18:48:35 -0700

    changed stop 'SilentlyContinue'

--- automatic/Cygwin/tools/chocolateyInstall.ps1
+++ automatic/Cygwin/tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'Stop'
+$ErrorActionPreference = 'SilentlyContinue'
 
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $pp = Get-PackageParameters
@@ -52,7 +52,4 @@ Install-BinFile -Name "Cygwin" -Path "$cygwin_root\Cygwin.bat"
 
 Write-Host "Copying cygwin package manager (setup) to $cygwin_root"
 $setup_path = if ((Get-OSArchitectureWidth 32) -or $env:ChocolateyForceX86) { $packageArgs.file } else { $packageArgs.file64 }
-New-Item -ItemType Directory -Path $cygwin_root
-$ErrorActionPreference = 'SilentlyContinue'
 Move-Item $setup_path $cygwin_root\cygwinsetup.exe -Force
-$ErrorActionPreference = 'Stop'
